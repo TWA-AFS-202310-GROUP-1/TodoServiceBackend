40693757dd00957861d6da9340f08a93df158053
Xinxin Xu <xxu37@slb.com>
2023-11-10 15:49:18 +0800

    fix: fix some data transfer errors in controller

--- ToDoService/Controllers/ToDoItemController.cs
+++ ToDoService/Controllers/ToDoItemController.cs
@@ -21,8 +21,8 @@ namespace ToDoService.Controllers
                 Title = "Home work",
                 Description = "Have to complete home work",
                 Id = currentId,
-                IsDone = false
-            }
+                IsDone = false,
+            },
         };
 
         [HttpGet("{id}")]
@@ -56,11 +56,11 @@ namespace ToDoService.Controllers
             return CreatedAtAction(nameof(Get), new { id = toDoItem.Id }, toDoItem);
         }
 
-        [HttpPut]
-        public ToDoItem Update(ToDoItem todoItem)
+        [HttpPut("{id}")]
+        public ToDoItem Update(long id, [FromBody]ToDoItem todoItem)
         {
             CheckAndThrowException();
-            var foundToDoItem = toDoItems.FirstOrDefault(item => item.Id == todoItem.Id);
+            var foundToDoItem = toDoItems.FirstOrDefault(item => item.Id == id);
             if (foundToDoItem != null)
             {
                 foundToDoItem.IsDone = true;
@@ -71,7 +71,7 @@ namespace ToDoService.Controllers
             return foundToDoItem;
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public ToDoItem Delete(long id)
         {
             CheckAndThrowException();
